430347f146b96bea21ff8dfc5dd4296242cc3a61
mgopalsa <42105370+mgopalsa@users.noreply.github.com>
2019-03-04 15:25:34 +1100

    Update settings.py

--- trelloapi/settings.py
+++ trelloapi/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'trelloapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config()}
 '''
 {
     'default': {
