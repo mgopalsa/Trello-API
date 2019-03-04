aa26dd3600cf5e0cd24f24771028bc518046d4f5
mgopalsa <42105370+mgopalsa@users.noreply.github.com>
2019-03-04 21:46:06 +1100

    Update admin.py

--- trello/admin.py
+++ trello/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Board, List, Card, Label
 
+admin.site.site_header = "Trello Admin"
+admin.site.site_title = "Trello Admin Portal"
+admin.site.index_title = "Welcome to Trello tool Portal"
+
 admin.site.register(Board)
 admin.site.register(List)
 admin.site.register(Card)
