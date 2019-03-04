7b271ea84c3a89b3f6334cf603436356ea4eaaf6
mgopalsa <42105370+mgopalsa@users.noreply.github.com>
2019-03-04 15:23:07 +1100

    Update settings.py

--- trelloapi/settings.py
+++ trelloapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,7 +77,9 @@ WSGI_APPLICATION = 'trelloapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {default': dj_database_url.config()}
+'''
+{
     'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -89,7 +92,7 @@ DATABASES = {
 
     }
 }
-
+'''
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
