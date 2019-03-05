419dd02d4fcf6866183fbe15f67a4852ef9b0a81
Manikandan Gopalsamy <manikandan.gopalsamy@gmail.com>
2019-03-05 18:52:47 +1100

    Swagger and Core API
    Include Swagger and Core API documentation

--- trello/urls.py
+++ trello/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, re_path
 from . import views
 from rest_framework.authtoken import views as authviews
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
 
+schema_view = get_swagger_view(title='Trello API')
 urlpatterns = [
     path('boards/', views.BoardList.as_view()),
     re_path(r'^boards/(?P<pk>[0-9]+)/$', views.BoardDetail.as_view()),
@@ -14,4 +17,6 @@ urlpatterns = [
     path("users/", views.UserCreate.as_view(), name="user_create"),
     path("login/", views.LoginView.as_view(), name="login"),
     path('api-token-auth/', authviews.obtain_auth_token),
+    path(r'swagger-docs/', schema_view),
+    path(r'docs/', include_docs_urls(title='Trello API')),
 ]
--- trelloapi/settings.py
+++ trelloapi/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'trello',
+    'rest_framework_swagger',
    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
